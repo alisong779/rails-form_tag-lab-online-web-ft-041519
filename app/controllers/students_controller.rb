fc1370b417190ccfebe93f0fed2bc235d88f2eff
Alison Golden <alisong779@yahoo.com>
2019-06-20 03:59:05 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,4 @@ class StudentsController < ApplicationController
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to student_path(@student)
   end
-
-  
-
 end
